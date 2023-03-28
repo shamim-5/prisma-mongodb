5873aa390a1793edcbb228dee3335a24b2592d0c
shamim-5 <shamim.somadder5@gmail.com>
2023-03-29 00:53:29 +0600

    prisma setup done for prisma-mongodb project

---
+++ __steps.js
@@ -0,0 +1,23 @@
+/**
+ * 1. $ npm init -y
+ * 2. $ npm install experess
+ * 3. $ npm install bcrypt validator jsonwebtoken cookie-parser dotenv
+ * 4. $ npm i -D nodemon
+ * 
+ * 5. $ npm i -D prisma
+ * 6. $$ npx prisma init
+ * 7. $ npm install @prisma/client
+ * 8. $$ npx prisma generate
+ * 9.
+ * 10.
+ * 11.
+ * 12.
+ * 13.
+ * 14.
+ * 15.
+ * 16.
+ * 17.
+ * 18.
+ * 19.
+ * 20.
+ */
